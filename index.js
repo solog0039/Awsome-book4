e33700420a9d7039b1119a44bbd6d5c280be2178
Solomon Kidanu <solog0039@gmail.com>
2023-07-03 22:51:14 +0300

    Awsome books projecte created

---
+++ index.js
@@ -0,0 +1,52 @@
+/* eslint-disable max-classes-per-file */
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class BookCollection {
+  constructor() {
+    const bookCollection = localStorage.getItem('bookCollection');
+    this.bookList = bookCollection ? JSON.parse(bookCollection) : [];
+  }
+
+  displayBooks() {
+    const bookListElement = document.getElementById('bookList');
+    bookListElement.innerHTML = '';
+    this.bookList.forEach((book, index) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book');
+      if (index % 2 !== 0) bookDiv.classList.add('grey');
+      bookDiv.innerHTML = `<p>"${book.title}" by ${book.author}</p><button class="removeButton" data-index="${index}">Remove Book</button>`;
+      bookListElement.appendChild(bookDiv);
+    });
+  }
+
+  addBook(title, author) {
+    const book = new Book(title, author);
+    this.bookList.push(book);
+    localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+    this.displayBooks();
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+
+  removeBook(title) {
+    this.bookList = this.bookList.filter((book) => book.title !== title);
+    localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+    this.displayBooks();
+  }
+}
+
+const myBookCollection = new BookCollection();
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  myBookCollection.addBook(title, author);
+});
